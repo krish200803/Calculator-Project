c494852bade593d04aaf14b9d906643bc31806fb
Umesh Bhadane <145696031+krish200803@users.noreply.github.com>
2025-08-25 15:15:43 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package Interface;
+
+public interface Calculator {
+	
+	double add (int a, int b);
+	
+	double div(int a, int b);
+	
+	double mul(int a, int b);
+	
+	double sub(int a, int b);
+
+}
